72478eccbb3263032d3533184a9fecaa4a0d72db
Jenifas <jenefa16@gmail.com>
2024-10-16 08:49:14 +0530

    Add files via upload
    Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Chrome {
+	
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean Isbeta = false;
+		int ReleaseYear = 2008;
+		char shortcutkey ='C';
+		
+				System.out.println("Chrome version = "  +(version));
+				
+				System.out.println("The Developer = "  +(developer));
+				
+				System.out.println(Isbeta);
+				
+				System.out.println("Released in the year = "  +(ReleaseYear));
+				
+				System.out.println("Shortcutkey = "  +(shortcutkey));
+				
+	}
+
+}
